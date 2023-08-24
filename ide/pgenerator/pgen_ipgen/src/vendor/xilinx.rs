5ffaf9760dc1cb6a84d03fc9a622128dd904476b
NakagamiYuta <guguru0014@gmail.com>
2023-08-24 17:53:07 +0900

    [change] bd.tcl, xgui.tclの記述部分をそれぞれ別モジュールとして分割

--- ide/pgenerator/pgen_ipgen/src/vendor/xilinx.rs
+++ ide/pgenerator/pgen_ipgen/src/vendor/xilinx.rs
@@ -1,3 +1,6 @@
+mod bd;
+mod xgui;
+
 use vfs::VfsPath;
 
 use crate::IPInfo;
@@ -9,125 +12,20 @@ impl Vendor for Xilinx {
     fn gen(_: &IPInfo, root: &mut VfsPath) -> anyhow::Result<()> {
         // TODO: component.xml
 
-        root.join("xgui")?
-            .create_dir()?;
-        root.join("xgui/xgui.tcl")?
-            .create_file()?
-            .write_all(xgui_tcl().as_bytes())?;
-
         root.join("bd")?
             .create_dir()?;
         root.join("bd/bd.tcl")?
             .create_file()?
-            .write_all(bd_tcl().as_bytes())?;
+            .write_all(bd::BD_TCL)?;
+
+        root.join("xgui")?
+            .create_dir()?;
+        root.join("xgui/xgui.tcl")?
+            .create_file()?
+            .write_all(xgui::XGUI_TCL)?;
 
         Ok(())
     }
-}
-
-fn xgui_tcl() -> &'static str {
-    r##"
-proc init_gui { IPINST } {
-    ipgui::add_param $IPINST -name "Component_Name"
-    ipgui::add_param $IPINST -name "START_ADDR"
-    ipgui::add_page $IPINST -name "Page 0"
-}
-
-proc update_PARAM_VALUE.START_ADDR { PARAM_VALUE.START_ADDR } {
-
-}
-
-proc validate_PARAM_VALUE.START_ADDR { PARAM_VALUE.START_ADDR } {
-	return true
-}
-
-proc update_MODELPARAM_VALUE.START_ADDR { MODELPARAM_VALUE.START_ADDR PARAM_VALUE.START_ADDR } {
-	set_property value [get_property value ${PARAM_VALUE.START_ADDR}] ${MODELPARAM_VALUE.START_ADDR}
-}
-    "##
-}
-
-fn bd_tcl() -> &'static str {
-    r##"
-proc init { cellpath otherInfo } {
-	set cell_handle [get_bd_cells $cellpath]
-	set all_busif [get_bd_intf_pins $cellpath/*]
-	set axi_standard_param_list [list ID_WIDTH AWUSER_WIDTH ARUSER_WIDTH WUSER_WIDTH RUSER_WIDTH BUSER_WIDTH]
-	set full_sbusif_list [list  ]
-
-	foreach busif $all_busif {
-		if { [string equal -nocase [get_property MODE $busif] "slave"] == 1 } {
-			set busif_param_list [list]
-			set busif_name [get_property NAME $busif]
-			if { [lsearch -exact -nocase $full_sbusif_list $busif_name ] == -1 } {
-			    continue
-			}
-			foreach tparam $axi_standard_param_list {
-				lappend busif_param_list "C_${busif_name}_${tparam}"
-			}
-			bd::mark_propagate_only $cell_handle $busif_param_list
-		}
-	}
-}
-
-proc pre_propagate { cellpath otherInfo } {
-	set cell_handle [get_bd_cells $cellpath]
-	set all_busif [get_bd_intf_pins $cellpath/*]
-	set axi_standard_param_list [list ID_WIDTH AWUSER_WIDTH ARUSER_WIDTH WUSER_WIDTH RUSER_WIDTH BUSER_WIDTH]
-
-	foreach busif $all_busif {
-		if { [string equal -nocase [get_property CONFIG.PROTOCOL $busif] "AXI4"] != 1 } {
-			continue
-		}
-		if { [string equal -nocase [get_property MODE $busif] "master"] != 1 } {
-			continue
-		}
-
-		set busif_name [get_property NAME $busif]
-		foreach tparam $axi_standard_param_list {
-			set busif_param_name "C_${busif_name}_${tparam}"
-
-			set val_on_cell_intf_pin [get_property CONFIG.${tparam} $busif]
-			set val_on_cell [get_property CONFIG.${busif_param_name} $cell_handle]
-
-			if { [string equal -nocase $val_on_cell_intf_pin $val_on_cell] != 1 } {
-				if { $val_on_cell != "" } {
-					set_property CONFIG.${tparam} $val_on_cell $busif
-				}
-			}
-		}
-	}
-}
-
-proc propagate { cellpath otherInfo } {
-	set cell_handle [get_bd_cells $cellpath]
-	set all_busif [get_bd_intf_pins $cellpath/*]
-	set axi_standard_param_list [list ID_WIDTH AWUSER_WIDTH ARUSER_WIDTH WUSER_WIDTH RUSER_WIDTH BUSER_WIDTH]
-
-	foreach busif $all_busif {
-		if { [string equal -nocase [get_property CONFIG.PROTOCOL $busif] "AXI4"] != 1 } {
-			continue
-		}
-		if { [string equal -nocase [get_property MODE $busif] "slave"] != 1 } {
-			continue
-		}
-
-		set busif_name [get_property NAME $busif]
-		foreach tparam $axi_standard_param_list {
-			set busif_param_name "C_${busif_name}_${tparam}"
-
-			set val_on_cell_intf_pin [get_property CONFIG.${tparam} $busif]
-			set val_on_cell [get_property CONFIG.${busif_param_name} $cell_handle]
-
-			if { [string equal -nocase $val_on_cell_intf_pin $val_on_cell] != 1 } {
-				if { $val_on_cell_intf_pin != "" } {
-					set_property CONFIG.${busif_param_name} $val_on_cell_intf_pin $cell_handle
-				}
-			}
-		}
-	}
-}
-    "##
 }
 
 #[cfg(test)]
