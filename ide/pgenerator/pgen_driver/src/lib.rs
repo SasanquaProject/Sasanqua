ae1a59d0355deba70e1add98b2c116688a0f13d1
NakagamiYuta <guguru0014@gmail.com>
2023-08-24 17:47:29 +0900

    [add] bd.tcl, xgui.tclを吐けるようにした

--- ide/pgenerator/pgen_driver/src/lib.rs
+++ ide/pgenerator/pgen_driver/src/lib.rs
@@ -1,3 +1,7 @@
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
 use vfs::PhysicalFS;
 
 use ipgen::IPInfo;
@@ -10,8 +14,16 @@ where
     let name = name.into();
     let version = version.into();
 
-    let mut fs = PhysicalFS::new(name.clone()).into();
-    IPInfo::new(name, version).gen::<V>(&mut fs)?;
+    let out_dir = setup_env(name.clone())?;
+    let mut root = PhysicalFS::new(out_dir).into();
+    IPInfo::new(name, version).gen::<V>(&mut root)?;
 
     Ok(())
+}
+
+fn setup_env(name: String) -> anyhow::Result<PathBuf> {
+    let current_dir = env::current_dir().unwrap();
+    let out_dir = current_dir.join(name);
+    fs::create_dir(&out_dir)?;
+    Ok(out_dir)
 }
