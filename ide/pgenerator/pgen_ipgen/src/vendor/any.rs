4cbd8dd348ff0516b36d1b77ae0c786a60c24ea3
NakagamiYuta <guguru0014@gmail.com>
2023-08-25 03:22:00 +0900

    [add] vendor::Any 追加

---
+++ ide/pgenerator/pgen_ipgen/src/vendor/any.rs
@@ -0,0 +1,27 @@
+use vfs::VfsPath;
+
+use hwgen::sasanqua::bus::BusInterface;
+use hwgen::SasanquaT;
+
+use crate::utils::merge_vfs;
+use crate::vendor::Vendor;
+use crate::IPInfo;
+
+pub struct Any;
+
+impl<S, B> Vendor<S, B> for Any
+where
+    S: SasanquaT<B>,
+    B: BusInterface,
+{
+    fn gen(ipinfo: &IPInfo<S, B>, root: &mut VfsPath) -> anyhow::Result<()> {
+        root.join("src")?.create_dir()?;
+        merge_vfs(root, "src", ipinfo.sasanqua.gen()?)?;
+
+        root.join("README.txt")?
+            .create_file()?
+            .write_all(format!("Name: {}\nVersion: {}", ipinfo.name, ipinfo.version).as_bytes())?;
+
+        Ok(())
+    }
+}
