8300934bc88f7a0e23e1495c6d4a70ff50f442cf
NakagamiYuta <guguru0014@gmail.com>
2023-10-15 21:57:27 +0900

    [add] tools/ipgen/README.md

---
+++ hw_design/design/tools/ipgen/examples/xilinx/main.rs
@@ -0,0 +1,14 @@
+use vfs::PhysicalFS;
+
+use ipgen::IPInfo;
+use ipgen::vendor::Xilinx;
+
+fn main() -> anyhow::Result<()> {
+    let dst_dir = "./example_ip";
+
+    std::fs::create_dir(dst_dir)?;
+    let mut fs = PhysicalFS::new(dst_dir).into();
+
+    let ipinfo = IPInfo::new("Example", "0.1.0");
+    ipinfo.gen::<Xilinx>(&mut fs)
+}
