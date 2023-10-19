2fc08f51f69cae7f6450d5e45b20077f7d774d91
NakagamiYuta <guguru0014@gmail.com>
2023-10-19 14:46:23 +0900

    [add] New, Gen コマンド実装 (copgen_cui)

--- hw_design/design/cop/src/cui/src/cmd.rs
+++ hw_design/design/cop/src/cui/src/cmd.rs
@@ -1,3 +1,5 @@
+mod new;
+mod gen;
 mod interactive;
 
 use clap::{AppSettings, Parser, Subcommand};
@@ -14,12 +16,21 @@ pub struct App {
 #[derive(Subcommand)]
 #[allow(non_camel_case_types)]
 enum AppSub {
+    /// Output a configure template
+    New(new::NewCmd),
+
+    /// Generate Cop-IP
+    Gen(gen::GenCmd),
+
+    /// Run a intaractive shell
     Interactive(interactive::InteractiveCmd),
 }
 
 impl App {
     pub fn run() -> anyhow::Result<()> {
         let result: anyhow::Result<()> = match App::parse().sub {
+            AppSub::New(cmd) => cmd.run(),
+            AppSub::Gen(cmd) => cmd.run(),
             AppSub::Interactive(cmd) => cmd.run(),
         };
         match result {
