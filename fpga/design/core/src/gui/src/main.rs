029220207da1daeb329d9e3a4034a3e8af7e457d
NakagamiYuta <guguru0014@gmail.com>
2023-11-08 22:09:17 +0900

    [add] Slint の実行環境を整えた

--- fpga/design/core/src/gui/src/main.rs
+++ fpga/design/core/src/gui/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello World");
+slint::slint! {
+    export component MainWindow inherits Window {
+        width: 128px;
+        height: 128px;
+
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    MainWindow::new()?.run()?;
+    Ok(())
 }
