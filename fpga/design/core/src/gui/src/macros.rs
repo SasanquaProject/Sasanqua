b991b24ac1ce49af4807857ab0f8a8f33986f83f
NakagamiYuta <guguru0014@gmail.com>
2023-11-12 18:13:46 +0900

    [clean] main.rs に定義していたマクロを macros.rs に移動

---
+++ fpga/design/core/src/gui/src/macros.rs
@@ -0,0 +1,38 @@
+pub use std::thread;
+
+pub use slint::Weak;
+
+pub use crate::ui::input;
+
+#[macro_export]
+macro_rules! callback {
+    ($window:ident . $event:ident, $func:path) => {
+        let window_weak = $window.as_weak();
+        $window.$event(move || {
+            let ui = input::UI::from(window_weak.clone());
+            let window = window_weak.clone();
+            thread::spawn(move || {
+                invoke_from_event_loop!(window => (|window: Weak<MainWindow>| {
+                    window.unwrap().set_generating(true);
+                    window.unwrap().set_err_message("".into())
+                }));
+
+                let ui = $func(ui);
+
+                invoke_from_event_loop!(window => (|window: Weak<MainWindow>| {
+                    window.unwrap().set_generating(false);
+                    window.unwrap().set_path(ui.path.into());
+                    window.unwrap().set_err_message(ui.err_msg.into());
+                }));
+            });
+        });
+    };
+}
+
+#[macro_export]
+macro_rules! invoke_from_event_loop {
+    ($window:expr => $body:tt) => {
+        let window = $window.clone();
+        slint::invoke_from_event_loop(move || $body(window)).unwrap();
+    };
+}
--- fpga/design/core/src/gui/src/main.rs
+++ fpga/design/core/src/gui/src/main.rs
@@ -1,45 +1,11 @@
 mod event;
 mod ui;
-
-use std::thread;
-
-use slint::Weak;
+mod macros;
 
 use event::{generate, pick_folder};
-use ui::input;
+use macros::*;
 
 slint::include_modules!();
-
-macro_rules! callback {
-    ($window:ident . $event:ident, $func:path) => {
-        let window_weak = $window.as_weak();
-        $window.$event(move || {
-            let ui = input::UI::from(window_weak.clone());
-            let window = window_weak.clone();
-            thread::spawn(move || {
-                invoke_from_event_loop!(window => (|window: Weak<MainWindow>| {
-                    window.unwrap().set_generating(true);
-                    window.unwrap().set_err_message("".into())
-                }));
-
-                let ui = $func(ui);
-
-                invoke_from_event_loop!(window => (|window: Weak<MainWindow>| {
-                    window.unwrap().set_generating(false);
-                    window.unwrap().set_path(ui.path.into());
-                    window.unwrap().set_err_message(ui.err_msg.into());
-                }));
-            });
-        });
-    };
-}
-
-macro_rules! invoke_from_event_loop {
-    ($window:expr => $body:tt) => {
-        let window = $window.clone();
-        slint::invoke_from_event_loop(move || $body(window)).unwrap();
-    };
-}
 
 fn main() -> anyhow::Result<()> {
     let window = MainWindow::new()?;
