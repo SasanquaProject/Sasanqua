dda7453c43c5204a61fd47aa68a5f242560d25ea
NakagamiYuta <guguru0014@gmail.com>
2023-11-19 04:16:38 +0900

    [add] UI デザインを雑に決定

--- ide/front/gui/ui/build.rs
+++ ide/front/gui/ui/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     slint_build::compile("design/Main.slint").unwrap();
 
+    println!("cargo:rerun-if-changed=design/**/*.slint");
     println!("cargo:rerun-if-changed=design/*.slint");
     println!("cargo:rerun-if-changed=build.rs");
 }
