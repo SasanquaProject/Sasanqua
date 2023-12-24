ba672676873e7e141962a1389b4bc9fba853458a
NakagamiYuta <guguru0014@gmail.com>
2023-12-25 03:14:06 +0900

    [add] core_runner クレート仮実装

---
+++ ide/core/core_runner/src/lib.rs
@@ -0,0 +1,62 @@
+use std::sync::Arc;
+use std::thread;
+
+use command::Command;
+use ipc::{Parent, Sender, Receiver};
+
+pub trait Runnable: Send {
+    fn run(&self, sender: Sender<Command>, receiver: Receiver<Command>);
+}
+
+pub struct Core {
+    ipc_parent: Arc<Parent<Command>>,
+    subprocesses: Vec<Box<dyn Runnable>>,
+}
+
+impl Core {
+    pub fn new() -> Self {
+        Core {
+            ipc_parent: Parent::new(),
+            subprocesses: vec![],
+        }
+    }
+
+    pub fn add_subprocess<T>(mut self, body: T) -> Self
+    where
+        T: Runnable + 'static,
+    {
+        self.subprocesses.push(Box::new(body));
+        self
+    }
+
+    pub fn run(self) -> anyhow::Result<()> {
+        // Launch subprocess
+        let threads = self
+            .subprocesses
+            .into_iter()
+            .map(|subprocess| {
+                let child = self.ipc_parent.spawn_child();
+                let sender = Sender::from(&child);
+                let receiver = Receiver::from(&child);
+                thread::spawn(move || {
+                    subprocess.run(sender, receiver);
+                })
+            })
+            .collect::<Vec<_>>();
+
+        // Event loop
+        loop {
+            // Receive a message
+            if let Some(command) = self.ipc_parent.try_pop() {
+                println!("Received: {:?}", command);
+            }
+
+            // Check subprocess
+            if threads.iter().any(|thread| thread.is_finished()) {
+                break;
+            }
+        }
+
+        Ok(())
+    }
+}
